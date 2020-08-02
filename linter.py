029db442c962c149b9542cd67c07d6d767b420ba
Sandro Wrzalek <sandro.wrzalek@gmail.com>
2020-08-02 17:10:22 +0200

    format code according to PEP8

--- linter.py
+++ linter.py
@@ -2,11 +2,10 @@ from SublimeLinter.lint import Linter
 
 
 class Mdl(Linter):
-    cmd= ['mdl', '${temp_file}']
+    cmd = ['mdl', '${temp_file}']
     regex = r'^.+?:(?P<line>\d+): (?P<warning>(?P<message>[^`]*))'
     line_col_base = (1, 1)
     tempfile_suffix = 'md'
     defaults = {
         'selector': 'text.html.markdown'
     }
-
