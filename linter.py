8ce6fd26a6af7f50243f119c340389ca64e71480
Sandro Wrzalek <sandro.wrzalek@gmail.com>
2020-08-03 09:36:31 +0200

    add RubyLinter class; Mdl class inherits from RubyLinter

--- linter.py
+++ linter.py
@@ -1,7 +1,27 @@
 from SublimeLinter.lint import Linter
 
 
-class Mdl(Linter):
+class RubyLinter(Linter):
+    def context_sensitive_executable_path(self, cmd):
+        # The default implementation will look for a user defined `executable`
+        # setting.
+        success, executable = super().context_sensitive_executable_path(cmd)
+        if success:
+            return True, executable
+
+        gem_name = cmd[0] if isinstance(cmd, list) else cmd
+
+        if self.settings.get('use_bundle_exec', False):
+            return True, ['bundle', 'exec', gem_name]
+
+        rvm = self.which('rvm-auto-ruby')
+        if rvm:
+            return True, [rvm, '-S', gem_name]
+
+        return False, None
+
+
+class Mdl(RubyLinter):
     cmd = ['mdl', '${temp_file}']
     regex = r'^.+?:(?P<line>\d+): (?P<warning>(?P<message>[^`]*))'
     line_col_base = (1, 1)
